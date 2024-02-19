a1dd1d90af7e07ab667a7a0f189970f97e1e3ba4
MainaJoseph <mainajm254@gmail.com>
2024-02-19 07:17:53 +0300

    Added different gradient to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,11 @@
 "use client";
 
+import React, { useState, useEffect } from "react";
 import { twMerge } from "tailwind-merge";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { useRouter } from "next/navigation";
 import { FaUserAlt } from "react-icons/fa";
-import { useSupabaseClient } from '@supabase/auth-helpers-react';
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import { toast } from "react-hot-toast";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
@@ -20,16 +21,37 @@ interface HeaderProps {
   className?: string;
 }
 
-const Header: React.FC<HeaderProps> = ({
-  children,
-  className,
-}) => {
+const Header: React.FC<HeaderProps> = ({ children, className }) => {
+  const [gradientIndex, setGradientIndex] = useState(0);
   const player = usePlayer();
   const router = useRouter();
   const authModal = useAuthModal();
 
   const supabaseClient = useSupabaseClient();
   const { user } = useUser();
+
+  const gradients = [
+    "bg-gradient-to-b from-emerald-800 to-green-600",
+    "bg-gradient-to-b from-blue-500 to-purple-400",
+    "bg-gradient-to-b from-yellow-700 to-amber-700",
+    "bg-gradient-to-b from-rose-800 to-pink-600",
+    "bg-gradient-to-b from-indigo-800 to-purple-600",
+    "bg-gradient-to-b from-cyan-800 to-teal-600",
+    "bg-gradient-to-b from-gray-800 to-gray-600",
+    "bg-gradient-to-b from-red-800 to-red-600",
+    "bg-gradient-to-b from-fuchsia-800 to-purple-600",
+    "bg-gradient-to-b from-lime-800 to-yellow-600",
+    "bg-gradient-to-b from-cyan-800 to-blue-600",
+    "bg-gradient-to-b from-pink-800 to-rose-600",
+  ]; // Add more gradients as needed
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setGradientIndex((prevIndex) => (prevIndex + 1) % gradients.length);
+    }, 15000);
+
+    return () => clearInterval(interval);
+  }, [gradients.length]);
 
   const handleLogout = async () => {
     const { error } = await supabaseClient.auth.signOut();
@@ -38,26 +60,27 @@ const Header: React.FC<HeaderProps> = ({
 
     if (error) {
       toast.error(error.message);
+    } else {
+      toast.success("Logged Out!");
     }
-    else {
-      toast.success('Logged Out!')
-    }
-  }
+  };
 
   return (
     <div
-      className={twMerge(`
+      className={twMerge(
+        `
         h-fit 
-        bg-gradient-to-b 
-        from-emerald-800 
+        ${gradients[gradientIndex]} // Apply dynamic gradient
         p-6
+        transition-all duration-1000 
         `,
         className
-      )}>
+      )}
+    >
       <div className="w-full mb-4 flex items-center justify-between">
         <div className="hidden md:flex gap-x-2 items-center">
-          <button 
-            onClick={() => router.back()} 
+          <button
+            onClick={() => router.back()}
             className="
               rounded-full 
               bg-black 
@@ -71,8 +94,8 @@ const Header: React.FC<HeaderProps> = ({
           >
             <RxCaretLeft className="text-white" size={35} />
           </button>
-          <button 
-            onClick={() => router.forward()} 
+          <button
+            onClick={() => router.forward()}
             className="
               rounded-full 
               bg-black 
@@ -88,8 +111,8 @@ const Header: React.FC<HeaderProps> = ({
           </button>
         </div>
         <div className="flex md:hidden gap-x-2 items-center">
-          <button 
-            onClick={() => router.push('/')} 
+          <button
+            onClick={() => router.push("/")}
             className="
               rounded-full 
               p-2 
@@ -104,8 +127,8 @@ const Header: React.FC<HeaderProps> = ({
           >
             <HiHome className="text-black" size={20} />
           </button>
-          <button 
-            onClick={() => router.push('/search')} 
+          <button
+            onClick={() => router.push("/search")}
             className="
               rounded-full 
               p-2 
@@ -124,14 +147,11 @@ const Header: React.FC<HeaderProps> = ({
         <div className="flex justify-between items-center gap-x-4">
           {user ? (
             <div className="flex gap-x-4 items-center">
-              <Button 
-                onClick={handleLogout} 
-                className="bg-white px-6 py-2"
-              >
+              <Button onClick={handleLogout} className="bg-white px-6 py-2">
                 Logout
               </Button>
-              <Button 
-                onClick={() => router.push('/account')} 
+              <Button
+                onClick={() => router.push("/account")}
                 className="bg-white"
               >
                 <FaUserAlt />
@@ -140,8 +160,8 @@ const Header: React.FC<HeaderProps> = ({
           ) : (
             <>
               <div>
-                <Button 
-                  onClick={authModal.onOpen} 
+                <Button
+                  onClick={authModal.onOpen}
                   className="
                     bg-transparent 
                     text-neutral-300 
@@ -152,13 +172,12 @@ const Header: React.FC<HeaderProps> = ({
                 </Button>
               </div>
               <div>
-                <Button 
-                  onClick={authModal.onOpen} 
+                <Button
+                  onClick={authModal.onOpen}
                   className="bg-white px-6 py-2"
                 >
                   Log in
                 </Button>
-                
               </div>
             </>
           )}
@@ -167,6 +186,6 @@ const Header: React.FC<HeaderProps> = ({
       {children}
     </div>
   );
-}
+};
 
 export default Header;
